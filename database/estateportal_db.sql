8a5ee9f92b57949144a0d0248f5122bf2c7207b8
Sa-developer-c <raghdazaki39@gmail.com>
2025-05-04 17:28:53 +0300

    Update estateportal_db.sql

--- database/estateportal_db.sql
+++ database/estateportal_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2025 at 03:08 PM
+-- Generation Time: May 04, 2025 at 04:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -50,8 +50,8 @@ CREATE TABLE `agent_list` (
 
 INSERT INTO `agent_list` (`id`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `password`, `avatar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 'user', 'D', 'محمود', 'Male', '09123456789', 'qalqilia', 'user@gmail.com', '202cb962ac59075b964b07152d234b70', 'uploads/agents/1.jpg?v=1645241414', 1, 0, '2022-02-19 11:30:14', NULL),
-(2, 'salah', 'm', 'sa', 'Male', '0599999', 'Ramallah', 'salah@gmail.com', '202cb962ac59075b964b07152d234b70', 'uploads/agents/2.jpg?v=1745412738', 1, 0, '2025-04-23 15:52:18', NULL),
-(3, 'صلاح', 'خ', 'محمد', 'Male', '5555', 'nablus', 'mohammed@gmail.com', '202cb962ac59075b964b07152d234b70', 'uploads/agents/3.jpg?v=1745413422', 1, 0, '2025-04-23 16:03:42', NULL);
+(2, 'manar', 'm', 'ma', 'Female', '05999559', 'Ramallah', 'manar@gmail.com', '202cb962ac59075b964b07152d234b70', 'uploads/agents/2.jpg?v=1745412738', 1, 0, '2025-04-23 15:52:18', NULL),
+(3, 'sama', 's', 'sama', 'Female', '5555', 'nablus', 'sama@gmail.com', '202cb962ac59075b964b07152d234b70', 'uploads/agents/3.jpg?v=1745413422', 1, 0, '2025-04-23 16:03:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -104,12 +104,10 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`id`, `property_id`, `sender_id`, `receiver_id`, `message`, `created_at`) VALUES
-(1, 2, 3, 2, 'مرحبا', '2025-04-23 16:01:46'),
-(2, 2, 2, 3, 'هلا', '2025-04-23 16:02:08'),
-(3, 2, 1, 2, 'السلام عليكم', '2025-04-23 16:02:18'),
-(4, 2, 2, 1, 'وعليكم السلام', '2025-04-23 16:02:26'),
-(5, 2, 3, 2, 'لدي استفسار حول هذا العقار', '2025-04-23 16:06:06'),
-(6, 2, 2, 3, '...', '2025-04-23 16:06:18');
+(25, 5, 1, 2, 'السلام عليكم', '2025-04-27 18:22:32'),
+(26, 5, 2, 1, 'وعليكم السلام', '2025-04-27 18:22:46'),
+(27, 5, 2, 3, 'مرحبا', '2025-04-27 18:25:05'),
+(28, 5, 3, 2, 'هلا', '2025-04-27 18:25:12');
 
 -- --------------------------------------------------------
 
@@ -146,7 +144,23 @@ INSERT INTO `real_estate_amenities` (`real_estate_id`, `amenity_id`) VALUES
 (3, 11),
 (3, 5),
 (3, 12),
-(3, 6);
+(3, 6),
+(4, 7),
+(4, 2),
+(4, 1),
+(5, 10),
+(5, 7),
+(5, 9),
+(5, 2),
+(5, 3),
+(5, 1),
+(5, 11),
+(5, 5),
+(5, 12),
+(5, 6),
+(6, 10),
+(6, 7),
+(6, 6);
 
 -- --------------------------------------------------------
 
@@ -162,16 +176,21 @@ CREATE TABLE `real_estate_list` (
   `agent_id` int(30) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = Available, 2 = not Available',
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  `sold_date` datetime DEFAULT NULL,
+  `feedback` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `real_estate_list`
 --
 
-INSERT INTO `real_estate_list` (`id`, `code`, `name`, `type_id`, `agent_id`, `status`, `date_created`, `date_updated`) VALUES
-(2, '20250400002', 'Sample 102', 2, 2, 1, '2022-02-19 14:12:49', '2025-04-23 16:00:45'),
-(3, '20250400001', 'Sample Only 101', 3, 2, 1, '2022-02-19 16:00:19', '2025-04-23 16:00:57');
+INSERT INTO `real_estate_list` (`id`, `code`, `name`, `type_id`, `agent_id`, `status`, `date_created`, `date_updated`, `sold_date`, `feedback`) VALUES
+(2, '20250400002', 'مكتب للايجار السنوي', 2, 3, 1, '2022-02-19 14:12:49', '2025-05-03 04:08:37', NULL, 0),
+(3, '20250400003', 'بيت مستقل للبيع', 3, 3, 1, '2022-02-19 16:00:19', '2025-04-23 16:41:31', NULL, 0),
+(4, '20250400001', 'شقة للبيع', 1, 1, 0, '2025-04-26 23:22:08', '2025-04-27 01:47:45', '2025-04-27 00:12:24', 3),
+(5, '20250500002', 'بيت للبيع', 3, 2, 1, '2025-04-27 17:46:05', '2025-05-03 16:40:07', NULL, NULL),
+(6, '20250500003', 'Sample 102', 2, 2, 1, '2025-05-03 03:49:49', '2025-05-03 16:40:42', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -190,22 +209,46 @@ CREATE TABLE `real_estate_meta` (
 --
 
 INSERT INTO `real_estate_meta` (`real_estate_id`, `meta_field`, `meta_value`) VALUES
-(2, 'type', 'Retail or Office'),
-(2, 'purpose', 'For Rent'),
-(2, 'area', '150 sq. m'),
-(2, 'location', 'Over here Street, Here City, Anywhere, 2306'),
+(2, 'type', 'مكتب'),
+(2, 'purpose', 'للايجار'),
+(2, 'area', '150 متر مرقع'),
+(2, 'location', 'قلقيلية , هنا الشارع، هنا المدينة,..'),
 (2, 'sale_price', '10000'),
 (2, 'coordinates', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13505.219515583316!2d34.981513!3d32.1960145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d3b9048e7320d%3A0x8ed08c13e5d9dc07!2sQalqilya!5e0!3m2!1sen!2s!4v1745413164768!5m2!1sen!2s'),
-(2, 'description', '&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;Integer id sodales elit. Integer rutrum egestas mauris, sit amet volutpat enim egestas at. Vestibulum sit amet placerat leo. Donec accumsan, orci eget scelerisque facilisis, risus nunc rhoncus lorem, nec porttitor ligula massa eget leo. Nulla eget quam id diam rhoncus gravida in eu nunc. Nullam felis enim, lacinia sed turpis sed, pretium blandit libero. Cras ante risus, facilisis quis lobortis ac, imperdiet id nisl.&lt;/p&gt;&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;Quisque orci leo, pellentesque a metus sit amet, sagittis ultricies lectus. Vestibulum eros felis, fringilla tincidunt sagittis egestas, tempus ac eros. Donec ullamcorper scelerisque nisi, ac venenatis leo rhoncus ac. Aliquam sed ex a nisl scelerisque commodo. Donec massa dolor, maximus sit amet tellus at, dictum fringilla neque. Praesent est elit, malesuada quis semper eget, viverra at leo. Morbi sed lectus consequat, congue augue eget, rutrum quam. Quisque fermentum id tortor a iaculis. Aliquam erat volutpat. Proin eget ipsum facilisis, placerat velit quis, ultrices urna.&lt;/p&gt;'),
+(2, 'description', '&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;/p&gt;'),
 (2, 'thumbnail_path', 'uploads/thumbnails/2.jpg?v=1645251171'),
-(3, 'type', 'Single-Family (Detached)'),
-(3, 'purpose', 'For Sale'),
-(3, 'area', '350 sq. m.'),
-(3, 'location', 'Sample'),
+(3, 'type', 'بيت لعائلة'),
+(3, 'purpose', 'للبيع'),
+(3, 'area', '350'),
+(3, 'location', 'رام الله'),
 (3, 'sale_price', '300000'),
 (3, 'coordinates', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13505.219515583316!2d34.981513!3d32.1960145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d3b9048e7320d%3A0x8ed08c13e5d9dc07!2sQalqilya!5e0!3m2!1sen!2s!4v1745413164768!5m2!1sen!2s'),
-(3, 'description', '&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;Sed ac pellentesque arcu, in facilisis neque. Aliquam erat volutpat. Suspendisse imperdiet odio ut neque condimentum, sed hendrerit lectus eleifend. Ut tempor molestie dui, laoreet tristique quam scelerisque at. Nulla elementum viverra ipsum, pharetra congue erat malesuada non. Nam commodo nisl vel sapien tincidunt tempor. Fusce vitae mi at enim convallis efficitur sed in justo.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;Aliquam nisi quam, interdum at aliquet vel, vehicula vitae justo. Vestibulum lobortis purus a urna condimentum auctor. Nulla venenatis pellentesque tortor nec viverra. Duis aliquet convallis tellus accumsan egestas. Integer maximus vitae magna a interdum. Nunc laoreet justo fermentum dui iaculis tristique. Nam consectetur vel elit id convallis.&lt;/p&gt;&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;Vivamus vestibulum id arcu non pulvinar. Donec ut metus sollicitudin, volutpat dui ut, faucibus turpis. Pellentesque rhoncus, eros sed suscipit dictum, dui risus elementum nisi, ut tempor erat libero a sapien. Nunc diam nibh, lobortis eget eros sit amet, vulputate tristique ipsum. Donec in augue massa. Nam pretium nisi in aliquam vehicula. Nam ligula est, accumsan sagittis sem at, vulputate volutpat nulla.&lt;/p&gt;'),
-(3, 'thumbnail_path', 'uploads/thumbnails/3.jpg?v=1645258133');
+(3, 'description', '&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 0.875rem;&quot;&gt;هنا وصف لتفاصيل اخرى تخص العقار .. .&amp;nbsp;&lt;/span&gt;&lt;/p&gt;'),
+(3, 'thumbnail_path', 'uploads/thumbnails/3.jpg?v=1645258133'),
+(4, 'type', 'سكني'),
+(4, 'purpose', 'بيع'),
+(4, 'area', '150'),
+(4, 'location', 'رام الله'),
+(4, 'sale_price', '55555'),
+(4, 'coordinates', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27095.778143476775!2d35.20588205!3d31.90735085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502d54cda2d58d1%3A0xbf6d4d17cc8b2c76!2z2LHYp9mFINin2YTZhNmH!5e0!3m2!1sar!2s!4v1745698800944!5m2!1sar!2s>'),
+(4, 'description', '&lt;p&gt;شقة جميلة للبيع بسعر جيد&lt;/p&gt;'),
+(4, 'thumbnail_path', 'uploads/thumbnails/4.jpeg?v=1745698928'),
+(5, 'type', 'سكني'),
+(5, 'purpose', 'بيع'),
+(5, 'area', '150'),
+(5, 'location', 'رام الله'),
+(5, 'sale_price', '55555'),
+(5, 'coordinates', 'https://maps.app.goo.gl/ov8m8YVXCUYqGuCM7'),
+(5, 'description', '....'),
+(5, 'thumbnail_path', 'uploads/thumbnails/5.jpg?v=1745765165'),
+(6, 'type', 'تجاري'),
+(6, 'purpose', 'ايجار'),
+(6, 'area', '350'),
+(6, 'location', 'الخليل - راس الجورة'),
+(6, 'sale_price', '10000'),
+(6, 'coordinates', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13505.219515583316!2d34.981513!3d32.1960145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d3b9048e7320d%3A0x8ed08c13e5d9dc07!2sQalqilya!5e0!3m2!1sen!2s!4v1745413164768!5m2!1sen!2s'),
+(6, 'description', '&lt;p&gt;مكتب تجاري ممتااز&lt;/p&gt;'),
+(6, 'thumbnail_path', 'uploads/thumbnails/6.jpeg?v=1746233389');
 
 -- --------------------------------------------------------
 
@@ -253,8 +296,7 @@ CREATE TABLE `type_list` (
 INSERT INTO `type_list` (`id`, `name`, `description`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
 (1, 'شقة', 'شقة', 1, 0, '2022-02-19 10:02:11', '2025-04-23 15:47:00'),
 (2, 'مكتب', 'مكتب', 1, 0, '2022-02-19 10:02:33', '2025-04-23 15:47:23'),
-(3, 'بيت', 'بيت', 1, 0, '2022-02-19 10:02:48', '2025-04-23 15:47:35'),
-(4, 'Raw land', 'Raw land typically refers to undeveloped or agricultural land such as farms, ranches, and timberlands. Many investors look at these properties as a good investment because they are tangible and finite resources. Additionally, these properties save you from the trouble of running renovations and worrying over stolen or damaged goods. ', 1, 0, '2022-02-19 10:03:35', NULL);
+(3, 'بيت', 'بيت', 1, 0, '2022-02-19 10:02:48', '2025-04-23 15:47:35');
 
 -- --------------------------------------------------------
 
@@ -280,7 +322,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', 'Admin', 'admin', '202cb962ac59075b964b07152d234b70', 'uploads/avatars/1.png?v=1645064505', NULL, 1, '2021-01-20 14:02:37', '2025-04-23 15:53:22');
+(1, 'Adminstrator', 'Admin', 'admin', '202cb962ac59075b964b07152d234b70', 'uploads/avatars/1.png?v=1645064505', NULL, 1, '2021-01-20 14:02:37', '2025-04-23 15:53:22'),
+(2, 'aya', 'admin', 'aya', '202cb962ac59075b964b07152d234b70', 'uploads/avatars/1.png?v=1645064505', NULL, 1, '2025-05-04 15:05:17', NULL),
+(3, 'raghd', 'admin', 'raghd', '202cb962ac59075b964b07152d234b70', 'uploads/avatars/1.png?v=1645064505', NULL, 1, '2025-05-04 15:05:17', NULL);
 
 --
 -- Indexes for dumped tables
@@ -363,13 +407,13 @@ ALTER TABLE `amenity_list`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `real_estate_list`
 --
 ALTER TABLE `real_estate_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `system_info`
@@ -387,7 +431,7 @@ ALTER TABLE `type_list`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
